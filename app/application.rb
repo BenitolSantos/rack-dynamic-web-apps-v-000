c676693505bcd0a3db734000130a5059e15fb530
Benito Lorenzo Santos <benitolsantos@gmail.com>
2018-09-28 20:57:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,6 +3,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
+    #Don't sweat the Kernel bit — Kernel is a module that holds many of Ruby's most useful bits
     num_1 = Kernel.rand(1..20)
     num_2 = Kernel.rand(1..20)
     num_3 = Kernel.rand(1..20)
